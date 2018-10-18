bb021a39760a83ce088c5a3169176f687e24f8be
better319 <better319@qq.com>
2018-10-18 10:10:46 +0800

    改名字

--- Termux_install_Ubuntu18.sh
+++
@@ -1,9 +0,0 @@
-echo 'Termux-Ubuntu18'
-mkdir ubuntu 
-cd ubuntu 
-wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
-bash ubuntu.sh
-cd ..
-cd ubuntu 
-./start-ubuntu.sh
-
